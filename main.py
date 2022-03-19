864d912891f23b01f4af57587a373ac2cf1c5a08
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-03-19 14:12:41 +0300

    All done

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import time
 
-def decor(foo):
+def decor_path(path=str):
+	def decor(foo):
+		def new_foo(*args, **kwars):
+			parametres = []
+			parametres.append(time.ctime())
+			result = foo(*args, **kwars)
+			parametres.append(f'{foo.__name__}')
+			parametres.append(args)
+			parametres.append(result)
+			with open(f'{path}log.txt', 'w', encoding='UTF-8') as f:
+				f.write(f'{parametres}')
+			return result
+		return new_foo
+	return decor
 
-	def new_foo(*args, **kwars):
-		parametres = []
-		parametres.append(time.ctime())
-		result = foo(*args, **kwars)
-		parametres.append(f'{foo.__name__}')
-		parametres.append(args)
-		parametres.append(result)
-		with open('log.txt', 'w', encoding='UTF-8') as f:
-			f.write(f'{parametres}')
-		return result
 
-	return new_foo
-
-@decor
+@decor_path('')
 def function(a, b):
 	x = a + b
 	print(x)
 	return x
 
-function(2, 4)
+
+if __name__ == '__main__':
+	function(2, 4)
